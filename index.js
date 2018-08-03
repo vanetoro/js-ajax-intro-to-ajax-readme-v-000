376b9d0b4fc4f8c076cf01e51d5f147fe73f77b6
Vanessa Toro <vanetoro84@gmail.com>
2018-08-03 15:37:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 function showRepositories(event, data){
   var repos = JSON.parse(this.responseText)
   console.log(repos)
-  const repoList = `<ul>${repos.map(r => '<li>' + r.name + '</li>').join('')}</ul>`
+  const repoList = `<ul>${repos.map(r => '<li>' + r.name + ' - <a href="#" data-repo="' + r.name + '" onclick="getCommits(this)">Get Commits</a></li>').join('')}</ul>`
   document.getElementById("repositories").innerHTML = repoList
 }
 
@@ -11,5 +11,19 @@ function getRepositories(){
   const req = new XMLHttpRequest()
   req.addEventListener('load', showRepositories)
   req.open('GET', 'https://api.github.com/users/octocat/repos')
+  req.send()
+}
+
+function showCommits(){
+  const commits = JSON.parse(this.responseText)
+  const commitsList = `<ul>${commits.map(commit => '<li><strong>' + commit.author.login + '</strong> - ' + commit.commit.message + '</li>'). join(' ')}</ul>`
+  document.getElementById("commits").innerHTML = commitsList
+}
+
+function getCommits(eld){
+  const name = el.dataset.repo
+  const req = new XMLHttpRequest()
+  req.addEventListener("load", showCommits())
+  req.open("Get", 'https://api.github.com/repos/octocat/' + name + '/commits' )
   req.send()
 }
