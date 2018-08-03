d2ad3e6004fb0a1f477ff22cd06ea659e9fc16e7
Vanessa Toro <vanetoro84@gmail.com>
2018-08-03 15:07:17 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,15 @@
+
+
+function showRepositories(event, data){
+  var repos = JSON.parse(this.responseText)
+  console.log(repos)
+  const repoList = `<ul>${repos.map(r => '<li>' + r.name + '</li>').join('')}</ul>`
+  document.getElementById("repositories").innerHTML = repoList
+}
+
+function getRepositories(){
+  const req = new XMLHttpRequest()
+  req.addEventListener('load', showRepositories)
+  req.open('GET', 'https://api.github.com/users/octocat/repos')
+  req.send()
+}
